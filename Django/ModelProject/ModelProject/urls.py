f5939816e0829395d50905f1a6198819dcc630be
mt6979 <1437255447@qq.com>
2018-06-02 10:57:31 +0800

    Django模型类示例，新加了注释。。。。

--- Django/ModelProject/ModelProject/urls.py
+++ Django/ModelProject/ModelProject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+#大家好
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myApp/',include('myApp.urls'))
